8beaaa3c33dd85a5740c54c1aece29c4a04696eb
ngocquang0651 <1712707@student.hcmus.edu.vn>
2022-07-11 17:44:20 +0700

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useContext} from "react";
 import type { RouteObject } from 'react-router-dom';
 import './App.css';
 import {
@@ -13,31 +13,68 @@ import CoursesIndex from "./templates/layouts/CoursesIndex";
 import NoMatch from "./templates/layouts/NoMatch";
 import Courses from "./templates/layouts/Courses";
 
-function App() {
-  const routes: RouteObject[] = [
-    {
-      path: "/",
-      element: < Layout  />,
-      children: [
-        { index: true, element: <Home /> },
-        {
-          path: "/courses",
-          element: <Courses />,
-          children: [
-            { index: true, element: <CoursesIndex /> },
-            { path: "/courses/:id", element: <Course /> },
-          ],
-        },
-        { path: "*", element: <NoMatch /> },
-      ],
-    },
-  ];
-  const element = useRoutes(routes);
+// function App() {
+//   const routes: RouteObject[] = [
+//     {
+//       path: "/",
+//       element: < Layout  />,
+//       children: [
+//         { index: true, element: <Home /> },
+//         {
+//           path: "/courses",
+//           element: <Courses />,
+//           children: [
+//             { index: true, element: <CoursesIndex /> },
+//             { path: "/courses/:id", element: <Course /> },
+//           ],
+//         },
+//         { path: "*", element: <NoMatch /> },
+//       ],
+//     },
+//   ];
+//   const element = useRoutes(routes);
 
+//   return (
+//     <div className="App">
+//       {element}
+//     </div>
+//   );
+// }
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const ThemeContext = React.createContext(themes.light);
+
+function App() {
   return (
-    <div className="App">
-      {element}
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar(props:any) {
+  return (
+    <div>
+      <ThemedButton />
     </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      Tôi đang được style bởi theme context!
+    </button>
   );
 }
 
